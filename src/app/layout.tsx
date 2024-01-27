a1a10005cb13485dc21b6fe4d41be151a81c9cff
chann44 <44chansw@gmail.com>
2024-01-27 22:12:27 +0530

    feat: added site header and footer with a new theam icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MainNav } from "@/components/site-header";
+import { SiteFooter } from "@/components/site-footer";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "container min-h-screen flex flex-col justify-between",
+          inter.className
+        )}
+      >
+        <MainNav />
+        {children}
+        <SiteFooter />
+      </body>
     </html>
   );
 }
---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "",
+  description: "",
+  links: {
+    github: "",
+    twitter: "",
+  },
+};
